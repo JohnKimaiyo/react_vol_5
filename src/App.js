9c11903346e72e0b14ee709f5ff7bfa1166d84ab
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-05 09:03:29 +0300

    welcome class component rendered

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './App.css';
 import Greet from './component/Greet';
+import Welcome from './component/Welcome';
 function App() {
   return (
     <div className="App">
-     <Greet/>
+      <Greet />
+      <Welcome />
     </div>
   );
 }
