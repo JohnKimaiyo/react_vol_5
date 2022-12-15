c519e5c8913c15a1ba07b1c83cd4395fe0e75d86
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-15 10:06:31 -0800

    theme constext api

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import Navbar from "./component/Navbar";
 
 import "./App.css";
 import BookList from "./component/BookList";
+import ThemeContextProvider from "./context/ThemeProvider";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <BookList />
+      <ThemeContextProvider>
+        <Navbar />
+        <BookList />
+      </ThemeContextProvider>
     </div>
   );
 }
